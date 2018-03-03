94fccc520b53b8fadd1b49ae30ab70e6e8eb7aa2
antoniolatela <alatel01@dcs.bbk.ac.uk>
2018-03-03 13:13:01 +0000

    inital commit

---
+++ src/Crypt.scala
@@ -0,0 +1,12 @@
+class Crypt {
+  List l =
+}
+
+
+object Crypt extends App {
+  override def main(args: Array[String]): Unit = {
+    args.foreach(println)
+    print(args(0))
+    var c = new Crypt();
+  }
+}
