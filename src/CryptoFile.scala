996d29d1ea7248003bebaba0f60a66b90b7e7887
antoniolatela <alatel01@dcs.bbk.ac.uk>
2018-03-05 20:22:55 +0000

    Added more functions and control at runtime

--- src/CryptoFile.scala
+++ src/CryptoFile.scala
@@ -15,28 +15,28 @@ class CryptoFile (method:String, key:String, fin:String, fout:String) {
     .toString)
 
   method match {
-    case "-e"  => CryptDecrypt(new CryptoImplementation("encrypted").CryptDecrypt, ABC, encABC, fin, fout) //call lambda passing CryptoImplementation class
-    case "-d"  => CryptDecrypt(new CryptoImplementation("decrypted").CryptDecrypt, encABC, ABC, fin, fout) //call lambda passing CryptoImplementation class
+    case "-e"  => CryptDecrypt(new CryptoFileImplementation("encrypted").CryptDecrypt, ABC, encABC, fin, fout) //call lambda passing CryptoImplementation class
+    case "-d"  => CryptDecrypt(new CryptoFileImplementation("decrypted").CryptDecrypt, encABC, ABC, fin, fout) //call lambda passing CryptoImplementation class
     case _ => throw new CryptoFileException("[ERROR]: Method not recognized -> use -e to encrypt and -d to decrypt")
   }
   //end of constructor
 
-  def CryptDecrypt(f: (String, String, String, String) //lambda function to call the decrypt method
-    => Unit, v0: String, v1: String, v3: String, v4: String):Unit = f(v0, v1, v3, v4)
+  //lambda function to call the decrypt method
+  def CryptDecrypt(f:(String*) => Boolean, s:String*):Boolean = f(s(0), s(1), s(2), s(3))
 }
 
-class CryptoImplementation(e:String){//constructor take method description type
-  def CryptDecrypt(s0:String, s1:String, s3:String, s4:String):Boolean = {//depending on encrypt\decrypt the method takes alphabet, encrypted alphabet, input file, output file //or encrypted alphabet, alphabet, input file, output file
-    val out = new PrintWriter(new File(s4)) //open file to write
+class CryptoFileImplementation(e:String){//constructor take method description type
+  def CryptDecrypt(s:String*):Boolean = {//depending on encrypt\decrypt the method takes alphabet, encrypted alphabet, input file, output file //or encrypted alphabet, alphabet, input file, output file
+    val out = new PrintWriter(new File(s(3))) //open file to write
       try {
-        Source.fromFile(s3).getLines().foreach(line => {line.foreach(a => {
+        Source.fromFile(s(2)).getLines().foreach(line => {line.foreach(a => {
                 val isUpper = a.isUpper//for each line perform conversion from one alphabet to another
-                if (s0.indexOf(a.toLower) != (-1)) { //check if char is in alphabet
-                  if (isUpper) {out.write(s1(s0.indexOf(a.toLower)).toUpper)} //write decoded uppercase char
-                  else {out.write(s1(s0.indexOf(a)))}} //write decoded lowercase char
+                if (s(0).indexOf(a.toLower) != (-1)) { //check if char is in alphabet
+                  if (isUpper) {out.write(s(1)(s(0).indexOf(a.toLower)).toUpper)} //write decoded uppercase char
+                  else {out.write(s(1)(s(0).indexOf(a)))}} //write decoded lowercase char
                 else out.write(a)}) //write decoded unrecognized char
             out.write("\n")})
-        println(s"[INFO]: File %s $e in file %s".format(s3, s4)) //write output
+        println(s"[INFO]: File %s $e in file %s".format(s(2), s(3))) //write output
         true //return if ok
       } catch {
         case e: Exception => throw new CryptoFileException("[ERROR]: Problem converting file -> " + e.getMessage)
